590f8e8aa854e74d71d839c7cde9bea947504e42
Soumya Gupta <soumyagupta@Soumyas-MacBook-Air.local>
2022-10-05 11:40:08 +0530

    dusshera

--- amazon-scraping/data/modify_pm.py
+++ amazon-scraping/data/modify_pm.py
@@ -2,7 +2,8 @@
 import csv
 import pandas as pd
 
-df=pd.read_csv("data_02_10_2022_PM.csv")
-df['set']=df['set'].replace({"AM":"PM"})
-df.to_csv("data_02_10_2022_PM.csv",index=False)
+df=pd.read_csv("data_02_10_2022_AM.csv")
+df['set']=df['set'].replace({"PM":"AM"})
+#df['date']=df['date'].replace({"04_10_2022":"02_10_2022"})
+df.to_csv("data_02_10_2022_AM.csv",index=False)
 ##print(df)
--- flipkart-scraping/data/modify_pm.py
+++ flipkart-scraping/data/modify_pm.py
@@ -1,6 +1,8 @@
 import csv
 import pandas as pd
 
-df=pd.read_csv("data_02_10_2022_PM.csv")
+df=pd.read_csv("data_04_10_2022_PM.csv")
 df['set']=df['set'].replace({"AM":"PM"})
-df.to_csv("data_02_10_2022_PM.csv",index=False)
+#df['date']=df['date'].replace({"04_10_2022":"02_10_2022"})
+df.to_csv("data_04_10_2022_PM.csv",index=False)
+##print(df)
